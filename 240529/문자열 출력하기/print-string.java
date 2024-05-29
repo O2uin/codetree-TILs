15744813c28962639a68a4d635b2ca05e4da1e5d
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-05-29 23:41:04 +0900

    Title: 문자열 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+
+        while(n>0){
+            System.out.println("LeebrosCode");
+            n--;
+        }
+    }
+}
