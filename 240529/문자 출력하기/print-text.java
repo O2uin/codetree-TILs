f89179f38659ab088502dcd66d8c505be5f0a7df
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-05-29 23:38:48 +0900

    Title: 문자 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        String n=scan.nextLine();
+        int i=0;
+
+        while(i<8){
+            System.out.printf(n);
+            i++;
+        }
+    }
+}
