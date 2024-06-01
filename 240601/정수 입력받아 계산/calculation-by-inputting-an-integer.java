2f502de79477d868468c2eed166661dabcda0ee2
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-01 20:36:38 +0900

    Title: 정수 입력받아 계산, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240601/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+
+        System.out.println(a*2+3);
+    }
+}
