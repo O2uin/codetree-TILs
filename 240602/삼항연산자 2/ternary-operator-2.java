459b5f45ae66f506d8146da3081ffc11193fcb65
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-02 00:29:15 +0900

    Title: 삼항연산자 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240602/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+
+        if(a==1){
+            System.out.println("t");
+        }else{
+            System.out.println("f");
+        }
+    }
+}
