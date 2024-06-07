fdaf7fbce36b76a61b231e804c5603061e5c3d8e
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-07 14:19:42 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240607/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        String result="";
+
+        for(int i=0;i<n;i++){
+            int a=scan.nextInt();
+            if(a%2==0){
+                result=Integer.toString(a)+result+" ";
+            }
+        }
+
+        System.out.println(result);
+    }
+}
