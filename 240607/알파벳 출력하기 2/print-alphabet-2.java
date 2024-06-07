d423a3a27cfc8feaeb0a392143512bcfb1acc84c
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-07 13:34:50 +0900

    Title: 알파벳 출력하기 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240607/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        char a=65;
+
+        for(int i=0;i<n;i++){
+            //A~Z 아스키 =65~90 
+            for(int k=0;k<i;k++){
+                System.out.print("  ");
+            }
+            for(int j=n-i;j>0;j--){
+                System.out.printf("%s ",a);
+                a++;
+                if(a==90){
+                    a=65;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
