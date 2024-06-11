a5416f0da15ac114d4e9860c6a7954daccb2a6a7
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-11 10:22:20 +0900

    Title: 그 전 알파벳, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240611/그 전 알파벳/before-the-alphabet.java
+++ 240611/그 전 알파벳/before-the-alphabet.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scan=new Scanner(System.in);
         //그 뭐냐 아스키코드값? --를 하되 97=a아래로 가면 122으로 바꾸고 --
         char ap=scan.next().charAt(0);
-        if(ap<97){
+        if(ap==97){
             ap=122;
         }else{
             ap--;
