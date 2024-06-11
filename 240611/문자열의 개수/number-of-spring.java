fd087c944739308f506b3dbadbea9b034b4caedd
tkrhk <89894264+tkrhk@users.noreply.github.com>
2024-06-11 15:21:15 +0900

    Title: 문자열의 개수, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240611/문자열의 개수/number-of-spring.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int count=0;
+        String str="";
+        while(true){
+            String a=scan.nextLine();
+            if(count%2==0){
+                //홀수번째(0부터 시작하니까 이게 맞음)저장
+                str+=a+"\r\n";
+            }
+            if(a.equals("0")){
+                break;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+        System.out.print(str);
+    }
+}
