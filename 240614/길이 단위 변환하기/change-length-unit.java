078e2b294db9bac6e70c174972bf4e71fcfb24ad
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-14 13:06:53 +0900

    Title: 길이 단위 변환하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240614/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double ftToCm=30.48;
+        int miToCm=160934;
+
+        double ft=9.2;
+        double mi=1.3;
+
+        System.out.printf("%.1fft = %.1fcm\r\n",ft,ft*ftToCm);
+        System.out.printf("%.1fmi = %.1fcm",mi,mi*miToCm);
+    }
+}
