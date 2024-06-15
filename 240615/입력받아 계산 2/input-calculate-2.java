d9feb871c3b6ec0a89e857eca70f24a428c89304
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-15 15:06:38 +0900

    Title: 입력받아 계산 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240615/입력받아 계산 2/input-calculate-2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+
+        System.out.println(a*b);
+    }
+}
