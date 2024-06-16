04c4d236ac2432dd221f6755966ff745e0f11efd
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-16 21:11:25 +0900

    Title: 실수 입력받아 계산, Time: 185ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240616/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        double a=scan.nextDouble();
+        double b=scan.nextDouble();        
+        System.out.printf("%.2f",a+b);
+    }
+}
