db6021953a32b8f7089b42b54d7c02242c9de021
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-17 15:12:44 +0900

    Title: 문자 받아 출력, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        char c=scan.next().charAt(0);
+        System.out.println(c);
+    }
+}
