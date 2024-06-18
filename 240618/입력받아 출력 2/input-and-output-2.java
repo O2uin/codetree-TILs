87cb09d2a02dae0d5024be1e9b7047701704ccec
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-18 19:28:33 +0900

    Title: 입력받아 출력 2, Time: 231ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240618/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        scan.useDelimiter("-");
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+
+        System.out.printf("%d%d",a,b);
+
+    }
+}
