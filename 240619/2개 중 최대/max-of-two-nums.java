c76740be98b2b38b9bc9c23f4e504fb56bbd4e89
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-19 11:20:53 +0900

    Title: 2개 중 최대, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/2개 중 최대/max-of-two-nums.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        if(a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
