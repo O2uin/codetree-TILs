e079822c5401e7bc1cb505a54c4b846c1cc0e4fe
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-19 11:22:34 +0900

    Title: 삼항연산자, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        if(a==100){
+            System.out.println("pass");
+        }else{
+            System.out.println("failure");
+        }
+    }
+}
