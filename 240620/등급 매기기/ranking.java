99b23756865f2f63735d691e7724258fbdf4cd8f
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-20 09:08:12 +0900

    Title: 등급 매기기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240620/등급 매기기/ranking.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        if(n>=90){
+            System.out.println("A");
+        }else if(n>=80){
+            System.out.println("B");
+        }else if(n>=70){
+            System.out.println("C");
+        }else if(n>=60){
+            System.out.println("D");
+        }else{
+            System.out.println("F");
+        }
+    }
+}
