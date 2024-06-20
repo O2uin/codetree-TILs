8c098416205575d0dcdf065a3c4bc7c796af1bdb
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-20 09:14:25 +0900

    Title: 최대 2번의 연산, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240620/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        if(a%2==0){
+            a=a/2;
+        }
+        if(a%2!=0){
+            a=(a+1)/2;
+        }
+
+        System.out.println(a);
+    }
+}
