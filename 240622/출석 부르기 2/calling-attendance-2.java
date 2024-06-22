4071b24b7fcd98c7001742a861aba20342ad57a5
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-22 20:15:19 +0900

    Title: 출석 부르기 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        while(true){
+            int a=scan.nextInt();
+            if(a==1){
+                System.out.println("John");
+            }else if(a==2){
+                System.out.println("Tom");
+            }else if(a==3){
+                System.out.println("Paul");
+            }else if(a==4){
+                System.out.println("Sam");
+            }else{
+                System.out.println("Vacancy");
+                break;
+            }
+        }
+    }
+}
