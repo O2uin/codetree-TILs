80a71ad019d53134294ebb796d67a84cc208062c
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-22 20:03:36 +0900

    Title: 0 나올때까지 출력, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        while(true){
+            int a=scan.nextInt();
+            if(a==0) break;
+            System.out.println(a);
+        }
+    }
+}
