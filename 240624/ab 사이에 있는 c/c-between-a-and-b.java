4144d2b752c8758ae8632c1dd1af6682eeda577c
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-24 09:04:24 +0900

    Title: ab 사이에 있는 c, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240624/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        boolean okay=false;
+
+        for(int i=a;i<=b;i++){
+            if(i%c==0){
+                okay=true;
+            }
+        }
+
+        if(okay==true){
+            System.out.println("YES");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
