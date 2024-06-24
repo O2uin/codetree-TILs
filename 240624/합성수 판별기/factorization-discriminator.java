774789e2c6d35a208529550b0e700f11e662cd26
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-24 09:07:58 +0900

    Title: 합성수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240624/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,22 @@
+import java.util.*l
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        boolean okay=false;
+
+        for(int i=2;i<n-1;i++){
+            if(n%i==0){
+                okay=true;
+            }
+        }
+
+        if(okay){
+            System.out.println("C");
+        }else{
+            System.out.println("N");
+        }
+    }
+}
