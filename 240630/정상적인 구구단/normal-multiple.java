fbe0a2817a695c22dfc3001f3fd1d58162799c7c
O2uin <89894264+O2uin@users.noreply.github.com>
2024-06-30 14:33:58 +0900

    Title: 정상적인 구구단, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240630/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.printf("%d * %d = %d",i,j,i*j);
+                if(j==n) break;
+                System.out.print(", ");
+            }
+            System.out.println();
+        }
+    }
+}
