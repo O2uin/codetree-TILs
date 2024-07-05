d94d56d94f882b24fe6043f8f02263d302130e2b
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-06 01:28:06 +0900

    Title: 정사각형 출력, Time: 515ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240706/정사각형 출력/print-square.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.printf("%d ",cnt);
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
