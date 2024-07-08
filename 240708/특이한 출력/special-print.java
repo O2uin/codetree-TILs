d1dd1b203fc89a904eaf907169f92e18f948526e
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-08 13:19:55 +0900

    Title: 특이한 출력, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240708/특이한 출력/special-print.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.printf("(%d, %d) ",i,j);
+                if(i+j==4){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
