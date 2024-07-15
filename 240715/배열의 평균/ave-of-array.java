9a2c4c1cff370bca19187f97258cf9cc21c056b0
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-15 16:36:09 +0900

    Title: 배열의 평균, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/배열의 평균/ave-of-array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int num[][]=new int[2][4];
+        int lineSum[]=new int[2];
+        int sum[]=new int[4];
+        int sumAll=0;
+        for(int i=0;i<2;i++){
+            for(int j=0;j<4;j++){
+                num[i][j]=scan.nextInt();
+                lineSum[i]+=num[i][j];
+                sum[j]+=num[i][j];
+                sumAll+=num[i][j];
+            }
+        }
+
+        for(int k=0;k<2;k++){
+            System.out.printf("%.1f ",(double)lineSum[k]/4);
+        }System.out.println();
+        for(int x=0;x<4;x++){
+            System.out.printf("%.1f ",(double)sum[x]/2);
+        }System.out.println();
+        System.out.printf("%.1f",(double)sumAll/8);
+    }
+}
