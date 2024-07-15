8b69d22a2abc6dabef7992a463f752571e2fe286
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-15 09:30:29 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int num[]=new int[n];
+        int min=-1;
+        int index=0;
+        //min 에 사서 max에 판매, 전체에서 min, max찾아다 빼기
+
+        for(int i=0;i<n;i++){
+            num[i]=scan.nextInt();
+            if(min==-1) min=num[i];
+            if(num[i]<min){
+                min=num[i];
+                index=i;
+            }
+        }
+        int max=num[index+1];
+
+        for(int j=index;j<n;j++){
+            if(num[j]>max){
+                max=num[j];
+            }
+        }
+
+        System.out.print(max-min);
+    }
+}
