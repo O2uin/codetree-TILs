b4b1239ffad5335a8ced06c428bbccde62e8877a
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-16 13:26:06 +0900

    Title: 문자열의 총 길이 구하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
+        int count=str.length()-9;
+        System.out.println(count);
+    }
+}
