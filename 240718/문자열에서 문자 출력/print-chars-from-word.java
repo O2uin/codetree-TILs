7ce891f029369f7c6b65adabd60a3c9858ad7a0f
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-18 10:05:46 +0900

    Title: 문자열에서 문자 출력, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240718/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        String a=scan.next();
+        for(int i=0;i<a.length();i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
