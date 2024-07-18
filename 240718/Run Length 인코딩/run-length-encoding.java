5b52e0eb9ba99482bd19790de617b499a291e03c
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-18 13:26:45 +0900

    Title: Run Length 인코딩, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240718/Run Length 인코딩/run-length-encoding.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        String a=scan.next();
+        String save="";
+        int count=1;
+
+        char str[]=new char[a.length()];
+        for(int i=0;i<a.length();i++){
+            str[i]=a.charAt(i);
+        }
+        save+=str[0];
+        for(int j=1;j<a.length();j++){
+            if(str[j-1]==str[j]){
+                count++;
+                if(j==a.length()-1){
+                    save+=count;
+                }
+            }else{
+                save+=String.valueOf(count);
+                count=1;
+                save+=str[j];
+            }
+            
+        }
+        
+        System.out.println(save.length()+"\r\n"+save);
+    }
+}
