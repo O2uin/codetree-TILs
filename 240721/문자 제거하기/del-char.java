6aaa9a80c24ae3cc7ab94eb84a1cabf1063d2cec
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-21 23:18:27 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240721/문자 제거하기/del-char.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        String str=scan.next();
+        while(true){
+            int a=scan.nextInt();
+            if(str.length()==2){
+                if(a==0) System.out.println(str.charAt(1));
+                else System.out.println(str.charAt(0));
+                break;
+            }
+            str=str.substring(0,a)+str.substring(a+1);
+            System.out.println(str);
+        }
+    }
+}
