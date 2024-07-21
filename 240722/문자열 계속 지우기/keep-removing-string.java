1591385e91185ddfa9136fd5ff5d68619dc6e8fa
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-22 01:02:10 +0900

    Title: 문자열 계속 지우기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/문자열 계속 지우기/keep-removing-string.java
+++ 240722/문자열 계속 지우기/keep-removing-string.java
@@ -29,7 +29,7 @@ public class Main {
                     }
                     a=str;
                     str="";
-                    i=0;
+                    i=-1;
                 }else{
                     ok=true;
                 }
