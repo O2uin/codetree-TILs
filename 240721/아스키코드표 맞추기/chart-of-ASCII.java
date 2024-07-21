870807a2f03a43fdc472479e628d6d1d31f45151
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-21 23:38:28 +0900

    Title: 아스키코드표 맞추기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            int a=scan.nextInt();
+            System.out.printf("%s ",(char)a);
+        }
+    }
+}
