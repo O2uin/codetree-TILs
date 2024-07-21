f995de2ba55c50277a23dd327287fb9d2c7220a5
O2uin <89894264+O2uin@users.noreply.github.com>
2024-07-21 23:40:30 +0900

    Title: 아스키코드 변환, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scan=new Scanner(System.in);
+        char a=scan.next().charAt(0);
+        int b=scan.nextInt();
+        System.out.printf("%d %s",(int)a, (char)b);
+    }
+}
